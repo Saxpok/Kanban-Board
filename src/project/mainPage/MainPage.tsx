814df99fea30d1ffb4b1b3fcfa779a5971dd3759
Maksym <mmsviridov17@gmail.com>
2024-03-14 11:38:17 +0200

    Debug: Css styles

--- src/project/mainPage/MainPage.tsx
+++ src/project/mainPage/MainPage.tsx
@@ -1,13 +1,19 @@
-import React from "react";
-import RepoInput from "../../ui/repoInput/RepoInput";
-import RepoButton from "../../ui/repoButton/RepoButton";
+import React, { useEffect } from "react";
 import RepoSearch from "./repoSearch/RepoSearch";
 import "./MainPage.style.css"
 import RepoInfo from "./repoInfo/RepoInfo";
 import TaskBlock from "./taskBlock/TaskBlock";
 
+const request = 'facebook/react'
 
 const MainPage = () => {
+    useEffect(() => {
+        fetch(`https://api.github.com/repos/${request}/issues`)
+            .then((res) => res.json())
+            .then(console.log)
+    })
+
+
     return (
         <div className="MainPage">
             <RepoSearch />
